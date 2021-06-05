ef779b8fe04cae975a162f3eaa4b5ebe17e21135
finesaaa <indriatifiqey16@gmail.com>
2021-06-05 12:56:12 +0700

    add t decrypt encrypt

--- SinSeiFS_B01.c
+++ SinSeiFS_B01.c
@@ -10,6 +10,106 @@
 
 static  const  char *dir_path = "/home/[user]/Downloads";
 char *log_path = "/home/[user]/SinSeiFS.log";
+char caesar[100] = "9(ku@AW1[Lmvgax6q`5Y2Ry?+sF!^HKQiBXCUSe&0M.b%rI'7d)o4~VfZ*{#:}ETt$3J-zpc]lnh8,GwP_ND|jO9(ku@AW1[L";
+
+void write_log(char *level, char *cmd_desc)
+{
+  FILE * fp;
+  fp = fopen (log_path, "a+");
+
+  time_t rawtime = time(NULL);
+  
+  struct tm *info = localtime(&rawtime);
+  
+  char time[100];
+  strftime(time, 100, "%d%m%y-%H:%M:%S", info);
+
+  char log[100];
+  sprintf(log, "%s::%s::%s\n", level, time, cmd_desc);
+  fputs(log, fp);
+
+  fclose(fp);
+}
+
+void encrypt(char *src) 
+{
+  int len = strlen(src);
+  int start = 0;
+
+  for (int i = strlen(src); i >= 0; i--) 
+  {
+    if(src[i] == '/')
+      break;
+
+    if(src[i] == '.')
+    {
+      len = i - 1;
+      break;
+    }
+  }
+
+  for (int i = 1; i < len; i++)
+    if (src[i] == '/')
+      start = i;
+
+  for (int i = start; i <= len; i++) 
+  {
+    if(src[i] == '/')
+      continue;
+
+    // int caesar_index = 0;
+    // while(1)
+    // {
+    //   if(src[i] == caesar[caesar_index])
+    //   {
+    //     src[i] = caesar[caesar_index + 10];
+    //     break;
+    //   }
+    //   caesar_index++;
+    // }
+  }
+}
+
+void decrypt(char *src) 
+{
+  int len = strlen(src); 
+  int start = 0;
+    
+  for (int i = 1; i < len; i++)
+  {
+    if(src[i] == '/' || src[i + 1] == '\0') 
+    {
+      start = i + 1;
+      break;
+    }
+  }
+
+  for (int i = strlen(src); i >= 0; i--)
+  {
+    if (src[i] == '.') 
+    {
+      len = i - 1;
+      break;
+    }
+  }
+
+  for (int i = start; i <= len; i++) 
+  {
+    if(src[i] == '/')
+      continue;
+
+    // int caesar_index = strlen(caesar) - 1;
+    // while(1)
+    // {
+    //   if(src[i] == caesar[caesar_index])
+    //   {
+    //     src[i] = caesar[caesar_index - 10];
+    //     break;
+    //   }
+    //   caesar_index--;
+    // }
+  }
+}
 
 static  int  xmp_getattr(const char *path, struct stat *stbuf)
 {
@@ -29,40 +129,62 @@ static  int  xmp_getattr(const char *path, struct stat *stbuf)
 
 static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
-    char fpath[1000];
+  char fpath[1000];
+  char tmp_path[1000];
 
-    if(strcmp(path,"/") == 0)
-    {
-        path=dir_path;
-        sprintf(fpath,"%s",path);
-    } else sprintf(fpath, "%s%s",dir_path,path);
+  if (strcmp(path, "/") == 0)
+  {
+    path = dir_path;
+    sprintf(fpath, "%s", path);
+  }
+  else
+  {
+    strcpy(tmp_path, path);
 
-    int res = 0;
+    if(strncmp(path, "/AtoZ_", 6) == 0)
+      decrypt(tmp_path);
 
-    DIR *dp;
-    struct dirent *de;
-    (void) offset;
-    (void) fi;
+    sprintf(fpath, "%s%s", dir_path, tmp_path);
+  }
 
-    dp = opendir(fpath);
+  int res = 0;
 
-    if (dp == NULL) return -errno;
+  DIR *dp;
+  struct dirent *de;
 
-    while ((de = readdir(dp)) != NULL) {
-        struct stat st;
+  (void)offset;
+  (void)fi;
 
-        memset(&st, 0, sizeof(st));
+  dp = opendir(fpath);
 
-        st.st_ino = de->d_ino;
-        st.st_mode = de->d_type << 12;
-        res = (filler(buf, de->d_name, &st, 0));
+  if(dp == NULL)
+    return -errno;
 
-        if(res!=0) break;
-    }
+  while ((de = readdir(dp)) != NULL)
+  {
+    if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0)
+      continue;
 
-    closedir(dp);
+    struct stat st;
+    memset(&st, 0, sizeof(st));
+    st.st_ino = de->d_ino;
+    st.st_mode = de->d_type << 12;
 
-    return 0;
+    char temp[1000];
+    strcpy(temp, de->d_name);
+
+    if (strncmp(path, "/AtoZ_", 6) == 0)
+      encrypt(temp);
+
+    res = (filler(buf, temp, &st, 0));
+
+    if (res != 0)
+      break;
+  }
+
+  closedir(dp);
+
+  return 0;
 }
 
 
@@ -112,8 +234,8 @@ static int xmp_mkdir(const char *path, mode_t mode)
     char temp[1000];
     strcpy(temp, path);
 
-    if(strncmp(path, "/encv1_", 6) == 0)
-      decryptV1(temp);
+    if(strncmp(path, "/AtoZ_", 6) == 0)
+      decrypt(temp);
 
     sprintf(fpath, "%s%s", dir_path, temp);
   }
@@ -124,7 +246,7 @@ static int xmp_mkdir(const char *path, mode_t mode)
 
   char desc[100];
   sprintf(desc, "MKDIR::%s", fpath);
-  writeLog("INFO", desc);
+  write_log("INFO", desc);
 
   return 0;
 }
@@ -145,8 +267,8 @@ static int xmp_unlink(const char *path)
     char temp[1000];
     strcpy(temp, path);
 
-    if(strncmp(path, "/encv1_", 6) == 0)
-      decryptV1(temp);
+    if(strncmp(path, "/AtoZ_", 6) == 0)
+      decrypt(temp);
 
     sprintf(fpath, "%s%s", dir_path, temp);
   }
@@ -157,7 +279,7 @@ static int xmp_unlink(const char *path)
 
   char desc[100];
   sprintf(desc, "REMOVE::%s", fpath);
-  writeLog("WARNING", desc);
+  write_log("WARNING", desc);
   
   return 0;
 }
@@ -178,8 +300,8 @@ static int xmp_rmdir(const char *path)
     char temp[1000];
     strcpy(temp, path);
 
-    if(strncmp(path, "/encv1_", 6) == 0)
-      decryptV1(temp);
+    if(strncmp(path, "/AtoZ_", 6) == 0)
+      decrypt(temp);
 
     sprintf(fpath, "%s%s", dir_path, temp);
   }
@@ -189,7 +311,7 @@ static int xmp_rmdir(const char *path)
 
   char desc[100];
   sprintf(desc, "RMDIR::%s", fpath);
-  writeLog("WARNING", desc);
+  write_log("WARNING", desc);
 
   return 0;
 }
@@ -209,8 +331,8 @@ static int xmp_rename(const char *from, const char *to)
     char temp[1000];
     strcpy(temp, from);
 
-    if(strncmp(from, "/encv1_", 6) == 0)
-      decryptV1(temp);
+    if(strncmp(from, "/AtoZ_", 6) == 0)
+      decrypt(temp);
 
     sprintf(ffrom, "%s%s", dir_path, temp);
   }
@@ -226,8 +348,8 @@ static int xmp_rename(const char *from, const char *to)
     char temp[1000];
     strcpy(temp, to);
 
-    if(strncmp(to, "/encv1_", 6) == 0)
-      decryptV1(temp);
+    if(strncmp(to, "/AtoZ_", 6) == 0)
+      decrypt(temp);
 
     sprintf(fto, "%s%s", dir_path, temp);
   }
@@ -238,7 +360,7 @@ static int xmp_rename(const char *from, const char *to)
 
   char desc[100];
   sprintf(desc, "RENAME::%s::%s", ffrom, fto);
-  writeLog("INFO", desc);
+  write_log("INFO", desc);
 
   return 0;
 }
@@ -259,8 +381,8 @@ static int xmp_truncate(const char *path, off_t size)
     char temp[1000];
     strcpy(temp, path);
 
-    if(strncmp(path, "/encv1_", 6) == 0)
-      decryptV1(temp);
+    if(strncmp(path, "/AtoZ_", 6) == 0)
+      decrypt(temp);
 
     sprintf(fpath, "%s%s", dir_path, temp);
   }
@@ -288,8 +410,8 @@ static int xmp_open(const char *path, struct fuse_file_info *fi)
     char temp[1000];
     strcpy(temp, path);
 
-    if(strncmp(path, "/encv1_", 6) == 0)
-      decryptV1(temp);
+    if(strncmp(path, "/AtoZ_", 6) == 0)
+      decrypt(temp);
 
     sprintf(fpath, "%s%s", dir_path, temp);
   }
@@ -316,8 +438,8 @@ static int xmp_write(const char *path, const char *buf, size_t size, off_t offse
     char temp[1000];
     strcpy(temp, path);
 
-    if(strncmp(path, "/encv1_", 6) == 0)
-      decryptV1(temp);
+    if(strncmp(path, "/AtoZ_", 6) == 0)
+      decrypt(temp);
 
     sprintf(fpath, "%s%s", dir_path, temp);
   }
@@ -338,7 +460,7 @@ static int xmp_write(const char *path, const char *buf, size_t size, off_t offse
 
   char desc[100];
   sprintf(desc, "WRITE::%s", fpath);
-  writeLog("INFO", desc);
+  write_log("INFO", desc);
 
   return res;
 }
